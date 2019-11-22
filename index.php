d440faff4b2e6f536d718d4dd3cfa4240d169df3
Jason <cardonajason1013@gmail.com>
2019-11-21 22:00:04 -0800

    starting files

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="keywords"
+        content="At Adore our goal is to provide you with the best lash experience possible. All adore staff takes pride in their work and  in making you feel  comfortable in a  relaxed environment.">
+
+    <meta property="og:title" content="Adore | Lashes & Beauty">
+    <meta property="og:image" content="https://adorelashesandbeauty.com/img/logo.gif">
+    <meta property="og:description" content="At Adore our goal is to provide you with the best lash experience possible. All adore staff takes pride in their work and  in making you feel  comfortable in a  relaxed environment.">
+    <meta property="og:url" content="https://adorelashesandbeauty.com">
+
+
+    <meta name="description" content="At Adore our goal is to provide you with the best lash experience possible. All adore staff takes pride in their work and  in making you feel  comfortable in a  relaxed environment.">
+
+    <meta name="author" content="Bluejdesigns.net:Jason Cardona & Alberto Sandoval">
+
+    <title>Adore | Lashes & Beauty</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    
+
+
+    <script src="js/main.js"></script>
+</body>
+</html>
